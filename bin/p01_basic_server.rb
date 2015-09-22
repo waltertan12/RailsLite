bcad888adfcfce639eaaf7bf170868603e74b519
Walter Tan <waltertan12@gmail.com>
2015-09-22 13:28:22 -0700

    Create basic server using WEBrick

---
+++ bin/p01_basic_server.rb
@@ -0,0 +1,16 @@
+require 'webrick'
+
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
+
+server = WEBrick::HTTPServer.new(Port: 3000)
+
+trap('INT') { server.shutdown }
+
+server.mount_proc("/") do |request, response|
+  response.body = request.path
+end
+
+server.start
