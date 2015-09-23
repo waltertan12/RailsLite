72beb3978f33a73949d8b057fa0f6fe3bd382309
Walter Tan <waltertan12@gmail.com>
2015-09-22 18:10:18 -0700

    Add params and routing features

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,11 +10,13 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
-      @params = Hash.new.merge(route_params)
+      @params = route_params
+      @params.merge!(parse_www_encoded_form(req.query_string))
+      @params.merge!(parse_www_encoded_form(req.body))
     end
 
     def [](key)
-      @params[key]
+      JSON.parse(@params.to_json)[key.to_s]
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -24,44 +26,43 @@ module Phase5
 
     class AttributeNotFoundError < ArgumentError; end;
 
-    # private
+    private
     # this should return deeply nested hash
     # argument format
     # user[address][street]=main&user[address][zip]=89436
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
-      query_array = URI.decode_www_form(www_encoded_form)
-      
-      query_array.each do |query|
-        parsed_key = parse_key(query[0])
-
-        if parsed_key.is_a?(String)
-          @params[parsed_key] = query[1]
-        else
-          index = 0
-          while index < parsed_key.length - 1
-            if index == parsed_key.length - 2
-              @params[parsed_key[index]] = { parsed_key[index + 1] => query.last }
-            else
-              @params[parsed_key[index]] = { parsed_key[index + 1] => nil }
-            end
-            index += 1
-          end
-        end
+      if www_encoded_form
+        hashed = Hash[URI.decode_www_form(www_encoded_form)]
+        nest(hashed)
+      else
+        {}
       end
+    end
+
+    def nest(hashed)
+      nested = {}
+
+      hashed.each do |key, val|
+        all_keys = parse_key(key)
+        current = nested
+
+        all_keys[0...-1].each do |nested_key|
+          current[nested_key] ||= {}
+          current = current[nested_key]
+        end
+
+        current[all_keys.last] = val
+      end
+
+      nested
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
-      if key =~ /\[|\]/
-        # puts "Key: #{key.split(/\]\[|\[|\]/)}"
-        key.split(/\]\[|\[|\]/)
-      else
-        # puts "Key: #{key}"
-        key
-      end
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
