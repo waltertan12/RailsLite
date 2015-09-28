c3c0e8bb850aebe20d7f168bafcb0ce000afee2a
Walter Tan <waltertan12@gmail.com>
2015-09-27 18:11:42 -0700

    Remove extra require_relatives

--- bin/console.rb
+++ bin/console.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
 require 'pry'
 require_relative './include'
-require_relative '../action_view/lib/action_view_manifest'
-require_relative '../active_record/lib/active_record_manifest'
 pry
