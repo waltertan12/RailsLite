a503677552ea23587cb6101a715b9fadee256e8f
Walter Tan <waltertan12@gmail.com>
2015-10-05 21:59:35 -0700

    Update Searchable module for ActiveRecordLite

--- active_record/lib/searchable.rb
+++ active_record/lib/searchable.rb
@@ -3,24 +3,5 @@ require_relative 'db_connection'
 module Searchable
   def where(params)
     Relation.new(self, self.table_name, params)
-    # values = []
-
-    # attributes = params.map do |attribute, value|
-    #   values << value
-    #   attribute.to_s + " = ?"
-    # end
-
-    # attributes_string = attributes.join(" AND ")
-
-    # found_items = DBConnection.execute2(<<-SQL, *values)
-    #   SELECT
-    #     *
-    #   FROM
-    #     #{table_name}
-    #   WHERE
-    #     #{attributes_string}
-    # SQL
-
-    # parse_all(found_items.drop(1))
   end
 end
