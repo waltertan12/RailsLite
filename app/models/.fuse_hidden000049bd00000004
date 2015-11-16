6e49142074063d7a1e3f8ca56cd9954f9b14c3bb
Walter Tan <waltertan12@gmail.com>
2015-11-16 14:53:34 -0700

    Update port for local server

---
+++ app/models/.fuse_hidden000049bd00000004
@@ -0,0 +1,6 @@
+class Human < ActiveRecordBase
+  self.table_name = "humans"
+  validates :name, presence: true
+
+  has_many :dogs
+end
--- bin/server.rb
+++ bin/server.rb
@@ -4,17 +4,15 @@ require 'webrick'
 require 'rack'
 require 'byebug'
 require_relative './include'
-
-router = ROUTER
  
 app = Proc.new do |env|
   req = Rack::Request.new(env)
   res = Rack::Response.new
-  router.run(req, res)
+  ROUTER.run(req, res)
   res.finish
 end
 
 Rack::Server.start(
   app: app,
-  port: 3000
+  Port: 3000
 )
